d580f8607998b1b90ae867380bad0f3dad6589a9
Syeda Sarah Shahrin <sarahshahrin123@gmail.com>
2024-02-26 18:49:22 -0500

    Initial Commit

---
+++ WaterDropSimulation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WaterDropSimulation
+//
+//  Created by Sarah Shahrin on 2/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WaterDropSimulation/WaterDropSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterDropSimulationApp.swift
+//  WaterDropSimulation
+//
+//  Created by Sarah Shahrin on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct WaterDropSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
